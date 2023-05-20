0a3ef6ada0b733f88218a3948def4568de0f0141
Brandon Liu <brandon@pinwheelapi.com>
2023-05-20 13:58:31 -0700

    Style header

--- sorting-articles/src/Article.js
+++ sorting-articles/src/Article.js
@@ -2,8 +2,9 @@
 import './Article.css';
 
 function Article(props) {
+  const headerClassName = props.isHeader ? "TableHeader" : ""
   return (
-    <div className="Article">
+    <div className={"Article " + headerClassName}>
       <div className="column half">{props.title}</div>
       <div className="column quarter">{props.upvotes}</div>
       <div className="column quarter">{props.date}</div>
--- sorting-articles/src/ArticleTable.js
+++ sorting-articles/src/ArticleTable.js
@@ -4,9 +4,13 @@ import Article from './Article';
 
 
 function ArticleTable ({articles}) {
+    let header = articles[0]
+
+    const header_row = <Article isHeader={true} title={header.title} upvotes={header.upvotes} date={header.date}></Article>
     return (
         <div className="ArticleTable">
-            {articles.map(article => <Article title={article.title} upvotes={article.upvotes} date={article.date}></Article>)}
+            {header_row}
+            {articles.slice(1).map(article => <Article title={article.title} upvotes={article.upvotes} date={article.date}></Article>)}
         </div>
     );
   
